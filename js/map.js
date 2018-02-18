5fc7f4f4503aea74dd0c07d6e9ddd2a900735c88
Tchernyavsky <tchernyavsky.daniil@yandex.ru>
2018-02-19 00:30:44 +0300

    simple drag'n'drop logic activated

--- js/map.js
+++ js/map.js
@@ -25,7 +25,9 @@
   var pins = [];
 
   var offsetX = pinMain.offsetLeft - MainPinParams.WIDTH * 0.5;
+  var primaryOffsetX = pinMain.offsetLeft;
   var offsetY = pinMain.offsetTop + MainPinParams.HEIGHT * 0.5 + MainPinParams.ARROW_HEIGHT;
+  var primaryOffsetY = pinMain.offsetTop;
 
   /**
    * Fill array of ads
@@ -71,11 +73,12 @@
    * Reset map to original state
    */
   var resetMap = function () {
-    window.form.fillAddress(offsetX, offsetY);
+    window.form.fillAddress(primaryOffsetX, primaryOffsetY);
+    pinMain.style.left = primaryOffsetX + 'px';
+    pinMain.style.top = primaryOffsetY + 'px';
     deletePins();
     map.classList.add('map--faded');
     form.classList.add('notice__form--disabled');
-    pinMain.addEventListener('mouseup', pinMainMouseupHandler);
   };
 
   var fragment = createPins(generateAdArray());
@@ -101,12 +104,45 @@
     });
   };
 
-  var pinMainMouseupHandler = function () {
-    enableMap();
-    pinMain.removeEventListener('mouseup', pinMainMouseupHandler);
-  };
+  pinMain.addEventListener('mousedown', function (evt) {
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
 
-  pinMain.addEventListener('mouseup', pinMainMouseupHandler);
+    pinMain.style.cursor = 'none';
+
+    var pinMainMouseMoveHandler = function (moveEvt) {
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+      pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+
+      offsetX = pinMain.offsetLeft - MainPinParams.WIDTH * 0.5;
+      offsetY = pinMain.offsetTop + MainPinParams.HEIGHT * 0.5 + MainPinParams.ARROW_HEIGHT;
+
+      window.form.fillAddress(offsetX, offsetY);
+    };
+
+    var pinMainMouseUpHandler = function () {
+      enableMap();
+      pinMain.style.cursor = 'move';
+
+      document.removeEventListener('mousemove', pinMainMouseMoveHandler);
+      document.removeEventListener('mouseup', pinMainMouseUpHandler);
+    };
+
+    document.addEventListener('mousemove', pinMainMouseMoveHandler);
+    document.addEventListener('mouseup', pinMainMouseUpHandler);
+  });
 
   window.resetMap = resetMap;
 })();
